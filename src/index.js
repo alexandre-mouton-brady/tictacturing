4fc9152b2cc4b0f6744b29541849c5af7930ed9b
unknown <amoutonbrady@gmail.com>
2017-11-19 16:20:28 +0100

    Project init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<div>Hello world</div>, document.getElementById('root'));
+registerServiceWorker();
